5492130188d477a22cf0bcc5b4958cd01c9d1ab8
Anita <anjogdand@dexciss.com>
2023-01-07 13:27:42 +0530

    Fix:warehouse barcode

--- barcode/custom_work_order.py
+++ barcode/custom_work_order.py
@@ -7,7 +7,8 @@ def barcode(item_code):
     if barcodes:
         return barcodes[0].barcode
     else:
-        barcode=frappe.db.sql("""select barcode from `tabItem Barcode`  where parent='{0}'  and  uom='' """.format(item_code),as_dict=1)
+        barcode=frappe.db.sql("""select barcode from `tabItem Barcode`  where parent='{0}'  and  uom is null""".format(item_code),as_dict=1)
+        print("&&&&&&&&&&&&&&&&&&&&&&&7777",barcode)
         if barcode:
             return barcode[0].barcode
 
